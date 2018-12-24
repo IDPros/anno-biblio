9d2cf47bf0eefcd2dc6fd6fd9da3ed7434d984d4
George Dobbs <george.dobbs@gmail.com>
2018-12-23 21:07:29 -0500

    compile.sh refreshes the annos/*.txt files

--- compile.sh
+++ compile.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 filename=$1
 filename="${filename%.*}"  # strip off any extension
-xelatex $filename
+
+# refresh the annotation files
+# the .bib files are the source
+cd bibs # outputs files into annos folder below bibs
+awk -f x-anno.awk *.bib
+cd ..
+
+# xelatex allows for modern fonts and unicode
+xelatex $filename # first run to prepare for biber
 biber $filename
-xelatex $filename
+xelatex $filename  # don't recall why there are two more, but it is needed.
 xelatex $filename
 open $filename.pdf
