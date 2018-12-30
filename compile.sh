8651a73e17f54347822e1c8953c7fb642376daba
George Dobbs <george.dobbs@gmail.com>
2018-12-30 15:36:46 -0500

    adj compile to sort the contributors

--- compile.sh
+++ compile.sh
@@ -13,6 +13,16 @@ cat annos/*.csv > ../contrib-cites.csv #put all into one file
 rm annos/*.csv #clean up
 cd ..
 
+# put the contributors file into order by last name
+head -n 1 contributors.csv > tmp
+awk -F ',' '{if(NR>1){print $0}}' contributors.csv | sort -t , -k 3 >> tmp
+[ -e contributors.old ] && rm contributors.old
+mv contributors.csv contributors.old
+mv tmp contributors.csv
+
+# regenerate the include file for the contributors
+awk -F ',' '{if(NR > 1) {print "\\addbibresource{bibs/" $1 ".bib}"}}' contributors.csv > bibs_index.tex
+
 # xelatex allows for modern fonts and unicode
 xelatex $filename # first run to prepare for biber
 biber $filename
